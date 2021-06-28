16f84d11de31cb065f876f244e3d5f340f51824f
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-06-28 09:23:37 +0100

    single product component

---
+++ src/components/single.vue
@@ -0,0 +1,466 @@
+<template>
+    <div>
+        <header class="single-header w-100">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <nav class="navbar navbar-expand-lg">
+              <a class="navbar-brand" href="#"
+                ><img
+                  class="d-md-block d-none"
+                  src="/logo.2da8e07c.png"
+                  alt="logo"
+              /></a>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="m-auto mb-2 mb-lg-0 navbar-nav">
+                  <li class="mx-3 nav-item">
+                    <a class="active nav-link" aria-current="page" href="#"
+                      >Home</a
+                    >
+                  </li>
+                  <li class="mx-3 nav-item">
+                    <a class="nav-link" href="#">Shop</a>
+                  </li>
+                  <li class="mx-3 nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                  </li>
+                  <li class="mx-3 nav-item">
+                    <a class="nav-link" href="#">About us</a>
+                  </li>
+                </ul>
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="cart-icon mx-4 nav-link text-primary" href="#"
+                      ><i class="fa fa-shopping-cart"></i
+                      ><span class="cart-badge">1</span></a
+                    >
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      class="btn-success nav-link px-4 rounded-pill shadow text-white"
+                      href="#"
+                      >Login</a
+                    >
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+    <main>
+      <section>
+        <div class="container">
+          <div class="row">
+            <div class="col-6 p-5">
+              <div class="m-0 row">
+                <div class="col-2">
+                  <div
+                    style="cursor:pointer;"
+                    class="mb-2 rounded shadow w-100"
+                  >
+                    <img
+                      src="/product-images-1.4c66486f.png"
+                      alt=""
+                      class="img img-responsive w-100"
+                    />
+                  </div>
+                  <div
+                    style="cursor:pointer;"
+                    class="mb-2 rounded shadow w-100"
+                  >
+                    <img
+                      src="/product-images-2.931f0e18.png"
+                      alt=""
+                      class="img img-responsive w-100"
+                    />
+                  </div>
+                  <div
+                    style="cursor:pointer;"
+                    class="mb-2 rounded shadow w-100"
+                  >
+                    <img
+                      src="/product-images-3.557947b4.png"
+                      alt=""
+                      class="img img-responsive w-100"
+                    />
+                  </div>
+                  <div
+                    style="cursor:pointer;"
+                    class="mb-2 rounded shadow w-100"
+                  >
+                    <img
+                      src="/product-images-4.9b16f918.png"
+                      alt=""
+                      class="img img-responsive w-100"
+                    />
+                  </div>
+                  <div
+                    style="cursor:pointer;"
+                    class="mb-2 rounded shadow w-100"
+                  >
+                    <img
+                      src="/product-images-5.84fd73ab.png"
+                      alt=""
+                      class="img img-responsive w-100"
+                    />
+                  </div>
+                </div>
+                <div class="col-10">
+                  <div style="cursor:pointer;" class="product-image rounded">
+                    <img
+                      src="/product.708685fd.png"
+                      class="img img-responsive shadow w-100"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-6 py-5">
+              <div class="path">
+                <a href="#">Home</a> > <a href="#">Shop</a> >
+                <a href="#">Dummy product</a>
+              </div>
+              <div class="p-title">
+                <h1 class="text-primary">Dummy product</h1>
+              </div>
+              <div class="p-short-desc">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus, nulla? Officia numquam perferendis commodi sequi
+                sapiente, repudiandae, sed non cupiditate accusamus minima
+                omnis, quidem nisi enim repellat debitis. Aliquid, doloribus!
+              </div>
+              <div class="price">
+                <h1 class="display-3 text-danger"><b>356$</b></h1>
+              </div>
+              <div class="align-items-center d-flex mb-3 stock">
+                <div class="m-2 pick">
+                  <span
+                    style="cursor:pointer;"
+                    class="bg-secondary m px-3 py-2 rounded subs text-white"
+                    >-</span
+                  >
+                  <span class="bg-light count m px-3 py-2 rounded text-dark"
+                    >1</span
+                  >
+                  <span
+                    style="cursor:pointer;"
+                    class="add bg-primary m px-3 py-2 rounded text-white"
+                    >+</span
+                  >
+                </div>
+                <div class="stock-count text-warning">50 in stock</div>
+              </div>
+              <div class="actions d-flex">
+                <a class="bg-blue btn btn-lg btn-primary m-2"
+                  ><i class="fa fa-shopping-cart"></i> Add to cart</a
+                >
+                <a class="btn btn-lg btn-warning m-2"
+                  ><i class="fa fa-shopping-cart"></i> Add to cart</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class="container mb-5"><hr class="bg-primary" /></div>
+      </section>
+      <section>
+        <div class="container">
+          <div class="row">
+            <div class="col text-primary">
+              <h1>Description:</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                optio reprehenderit iure mollitia quam magni ea ab eum. Magni
+                libero iusto eaque sint dignissimos facilis nisi quisquam
+                exercitationem assumenda quibusdam?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                optio reprehenderit iure mollitia quam magni ea ab eum. Magni
+                libero iusto eaque sint dignissimos facilis nisi quisquam
+                exercitationem assumenda quibusdam?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                optio reprehenderit iure mollitia quam magni ea ab eum. Magni
+                libero iusto eaque sint dignissimos facilis nisi quisquam
+                exercitationem assumenda quibusdam?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                optio reprehenderit iure mollitia quam magni ea ab eum. Magni
+                libero iusto eaque sint dignissimos facilis nisi quisquam
+                exercitationem assumenda quibusdam?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                optio reprehenderit iure mollitia quam magni ea ab eum. Magni
+                libero iusto eaque sint dignissimos facilis nisi quisquam
+                exercitationem assumenda quibusdam?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                optio reprehenderit iure mollitia quam magni ea ab eum. Magni
+                libero iusto eaque sint dignissimos facilis nisi quisquam
+                exercitationem assumenda quibusdam?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                optio reprehenderit iure mollitia quam magni ea ab eum. Magni
+                libero iusto eaque sint dignissimos facilis nisi quisquam
+                exercitationem assumenda quibusdam?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                optio reprehenderit iure mollitia quam magni ea ab eum. Magni
+                libero iusto eaque sint dignissimos facilis nisi quisquam
+                exercitationem assumenda quibusdam?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                optio reprehenderit iure mollitia quam magni ea ab eum. Magni
+                libero iusto eaque sint dignissimos facilis nisi quisquam
+                exercitationem assumenda quibusdam?
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class="container mb-5"><hr class="bg-primary" /></div>
+      </section>
+      <section class="mb-5">
+        <div class="container">
+          <div class="row">
+            <div class="col text-primary">
+              <h2>Comments:</h2>
+              <form
+                action="#"
+                method="post"
+                class="bg-light border px-2 py-5 rounded"
+              >
+                <div class="row">
+                  <div class="col-6 form-group mb-3">
+                    <input
+                      type="text"
+                      class="form-control form-control-lg"
+                      placeholder="Name..."
+                    />
+                  </div>
+                  <div class="col-6 form-group mb-3">
+                    <input
+                      type="text"
+                      class="form-control form-control-lg"
+                      placeholder="Email..."
+                    />
+                  </div>
+                </div>
+                <div class="form-group mb-3">
+                  <textarea
+                    name="#"
+                    id="#"
+                    class="form-control form-control-lg text-primary"
+                    placeholder="Comment..."
+                  ></textarea>
+                </div>
+                <div class="d-flex form-group mb-3">
+                  <button type="submit" class="btn btn-lg btn-primary px-5">
+                    Send
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class="container">
+          <div class="row">
+            <div class="col-12 mb-3">
+              <div class="m-0 row">
+                <div class="col-auto">
+                  <div class="comment-avatar">
+                    <img
+                      src="/avatar.1a6ff6ea.png"
+                      alt=""
+                      class="img img-responsive rounded-circle"
+                    />
+                  </div>
+                </div>
+                <div class="col text-primary">
+                  <div class="comment-details">
+                    <h2>Dummy username</h2>
+                    <span>dummyemail@email.com</span>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Facilis possimus dicta minus neque sit placeat expedita
+                      cumque debitis dolore doloremque nesciunt, illum provident
+                      obcaecati enim recusandae adipisci commodi, natus
+                      perferendis.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 mb-3">
+              <div class="m-0 row">
+                <div class="col-auto">
+                  <div class="comment-avatar">
+                    <img
+                      src="/avatar.1a6ff6ea.png"
+                      alt=""
+                      class="img img-responsive rounded-circle"
+                    />
+                  </div>
+                </div>
+                <div class="col text-primary">
+                  <div class="comment-details">
+                    <h2>Dummy username</h2>
+                    <span>dummyemail@email.com</span>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Facilis possimus dicta minus neque sit placeat expedita
+                      cumque debitis dolore doloremque nesciunt, illum provident
+                      obcaecati enim recusandae adipisci commodi, natus
+                      perferendis.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 mb-3">
+              <div class="m-0 row">
+                <div class="col-auto">
+                  <div class="comment-avatar">
+                    <img
+                      src="/avatar.1a6ff6ea.png"
+                      alt=""
+                      class="img img-responsive rounded-circle"
+                    />
+                  </div>
+                </div>
+                <div class="col text-primary">
+                  <div class="comment-details">
+                    <h2>Dummy username</h2>
+                    <span>dummyemail@email.com</span>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Facilis possimus dicta minus neque sit placeat expedita
+                      cumque debitis dolore doloremque nesciunt, illum provident
+                      obcaecati enim recusandae adipisci commodi, natus
+                      perferendis.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 mb-3">
+              <div class="m-0 row">
+                <div class="col-auto">
+                  <div class="comment-avatar">
+                    <img
+                      src="/avatar.1a6ff6ea.png"
+                      alt=""
+                      class="img img-responsive rounded-circle"
+                    />
+                  </div>
+                </div>
+                <div class="col text-primary">
+                  <div class="comment-details">
+                    <h2>Dummy username</h2>
+                    <span>dummyemail@email.com</span>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Facilis possimus dicta minus neque sit placeat expedita
+                      cumque debitis dolore doloremque nesciunt, illum provident
+                      obcaecati enim recusandae adipisci commodi, natus
+                      perferendis.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 mb-3">
+              <div class="m-0 row">
+                <div class="col-auto">
+                  <div class="comment-avatar">
+                    <img
+                      src="/avatar.1a6ff6ea.png"
+                      alt=""
+                      class="img img-responsive rounded-circle"
+                    />
+                  </div>
+                </div>
+                <div class="col text-primary">
+                  <div class="comment-details">
+                    <h2>Dummy username</h2>
+                    <span>dummyemail@email.com</span>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Facilis possimus dicta minus neque sit placeat expedita
+                      cumque debitis dolore doloremque nesciunt, illum provident
+                      obcaecati enim recusandae adipisci commodi, natus
+                      perferendis.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 mb-3">
+              <div class="m-0 row">
+                <div class="col-auto">
+                  <div class="comment-avatar">
+                    <img
+                      src="/avatar.1a6ff6ea.png"
+                      alt=""
+                      class="img img-responsive rounded-circle"
+                    />
+                  </div>
+                </div>
+                <div class="col text-primary">
+                  <div class="comment-details">
+                    <h2>Dummy username</h2>
+                    <span>dummyemail@email.com</span>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Facilis possimus dicta minus neque sit placeat expedita
+                      cumque debitis dolore doloremque nesciunt, illum provident
+                      obcaecati enim recusandae adipisci commodi, natus
+                      perferendis.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="css">
+    
+</style>
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Home from './components/home.vue'
 import Login from './components/login.vue'
 import Reset from './components/reset_pwd.vue'
 import VerifyEmail from './components/verify_email.vue'
+import single from './components/single.vue'
 
 const routes = [
     // {path : '/',component : page},
@@ -23,6 +24,10 @@ const routes = [
         path: '/verify_email',
         component: VerifyEmail
     },
+    {
+        path : '/single',
+        component: single
+    }
 ]
 
 export default routes
