0562dd68efc357f5806a1c0eab04c0f00b8449f4
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-06-29 13:46:07 +0100

    single product commnet component

---
+++ src/components/core-components/comment-card.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="col-12 mb-3">
+        <div class="m-0 row">
+        <div class="col-auto">
+            <div class="comment-avatar">
+            <img
+                src="/avatar.1a6ff6ea.png"
+                alt=""
+                class="img img-responsive rounded-circle"
+            />
+            </div>
+        </div>
+        <div class="col text-primary">
+            <div class="comment-details">
+            <h2>{{ name }}</h2>
+            <span>{{ email }}</span>
+            <p>
+                {{ comment }}
+            </p>
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        name:String,
+        email:String,
+        comment:String
+    }
+}
+</script>
+
+<style lang="css">
+    
+</style>
